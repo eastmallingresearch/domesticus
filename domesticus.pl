9ae827744a633971e229454476b3d7dbceda2273
Richard Harrison <richard.harrison@emr.ac.uk>
2014-04-19 11:55:08 +0100

    Got to a point mutation in the recognition site

--- domesticus.pl
+++ domesticus.pl
@@ -48,32 +48,198 @@ my @seqobjects=generate_objects(\@enzymes,\$ra, \$locatable_seq);
 recognition_sites(\@seqobjects, \$custom_collection);
 
 
+	
 #ONCE ALL COORDINATES IN CHECK CODONS
 
-my $sc=11;
-my $ec=32;
-
 #CONVERT COORDINATES
-#my @coord=coord_convert(\$locatable_seq,\$input_prot,\$sc,\$ec);
-#print "Converted coordinates ";
-#print join "\t", @coord;
-#print "\n";
+coord_convert(\@seqobjects);
+#print "Converted coordinates \n";
+
+foreach (@seqobjects){
+	#print $_->source_tag."\t";
+	my $tag=$_->source_tag;
+	#print $tag."\n";
+	#print $_->primary_tag."\t";
+	my @one= $_->get_tag_values('one');
+	my @two= $_->get_tag_values('two');
+	my @three= $_->get_tag_values('three');
+	my @four= $_->get_tag_values('four');
+	my @five= $_->get_tag_values('five');
+	my @six= $_->get_tag_values('six');
+	my @seven= $_->get_tag_values('seven');
+	my @eight= $_->get_tag_values('eight');
+	my @nine= $_->get_tag_values('nine');
+	my @ten= $_->get_tag_values('ten');
+	my @eleven= $_->get_tag_values('eleven');
+	
+	#print join "\t", @one;
+	#print "\t";
+	#print join "\t", @two;
+	#print "\t";
+	#print join "\t", @three;
+	#print "\t";#
+	#print join "\t", @four;
+	#print "\t";
+	#print $_->strand;
+	#print "\t";	
+	#print join "\t", @five;
+	#print "\t";
+	#print join "\t", @six;
+	#print "\t";
+	#print join "\t", @seven;
+	#print "\t";
+	#print join "\t", @eight;
+	#print "\t";
+	#print join "\t", @nine;
+	#print "\t";
+	#print join "\t", @ten;
+	#print "\t";
+	#print join "\t", @eleven;
+	#print "\n";
+	
+	
+	}
+	
 
 
+##MUTAGENESIS POSITION
 
-
+mutagenesis(\@seqobjects);
 
 
 
 
 #############SUBROUTINES##################
 
+########MUTAGENESIS##########
+
+sub mutagenesis{
+my ($seqs)=@_;
+
+
+#IN THIS SUBROUTINE, NOW THAT ALL THE COORDINATES ARE KNOWN AND THE FRAME OF THE 
+#CUT SITE IS UNDERSTOOD SYNONYMOUS CHANGES NEED TO BE FOUND THAT WILL ABOLISH THE CUT 
+#SITE
+use Bio::Tools::CodonTable;
+my $myCodonTable   = Bio::Tools::CodonTable->new();
+print  join (' ', "The name of the codon table no.", $myCodonTable->id(1),
+	       "is:", $myCodonTable->name(), "\n");
+	foreach (@$seqs){
+		my @one= $_->get_tag_values('one');
+		my @two= $_->get_tag_values('two');
+		my @three= $_->get_tag_values('three');
+		my @four= $_->get_tag_values('four');
+		my @five= $_->get_tag_values('five');
+		my @six= $_->get_tag_values('six');
+		my @seven= $_->get_tag_values('seven');
+		my @eight= $_->get_tag_values('eight');
+		my @nine= $_->get_tag_values('nine');
+		my @ten= $_->get_tag_values('ten');
+		my @eleven= $_->get_tag_values('eleven');
+		my @twelve= $_->get_tag_values('twelve');
+		my @split_eleven=split('',$eleven[0]);
+		my @split_twelve=split('',$twelve[0]);
+		my $counter=0; 
+	    print "\n\n\nCODON MUTATION FOR ".$_->primary_tag." BASES BEING CONSIDERED\t";
+		print scalar(@split_twelve)."\n";
+		
+		# FOR EACH AA SITE IN PEPTIDE FIND MUTATIONS
+		
+			for (my $i=0;$i<scalar(@split_twelve);$i=$i+3){	
+	 			my $codon =($split_twelve[$i].$split_twelve[$i+1].$split_twelve[$i+2]);
+	 			#print "CODON ".$codon."\n";
+	 			print "AMINO ACID ".$split_eleven[$counter]."\n";
+	 			#print "EXAMINING CODON TABLE\n";
+	 			my @codons = $myCodonTable->revtranslate($split_eleven[$counter]);
+  	 			#print join "\t", @codons;
+				#print "\n";
+				#IF WE ARE AT A SITE WITH >1 FOLD DEGENERACY THEN LIST THE MUTATIONS AND 
+				#THE POSITION OF THE MUTATION IN AN ARRAY
+				if (scalar @codons>1){
+					#PROVIDE THIS SUBROUTINE WITH DETAILS OF PEPTIDE, RECOGNITION SITE AND POSITION IN CURRENT LOOP
+					my @mutations=mutator(\@codons,\$codon,\$i,\$three[0],\$four[0],\$seven[0],\$eight[0]);
+					print "MUTATION AT SITE WITHIN RECOGNITION SEQUENCE ".$counter."\n";
+					foreach(@mutations){
+						my @tmp=$_;
+							foreach (@tmp){
+								print $_."\t";
+							}
+							
+						}
+						print "\n";
+						
+					}
+				$counter=$counter+1;
+			}
+			
+		###NOW YOU HAVE A PILE OF POSSIBLE MUTATIONS, PICK ONE AND ADD COORDINATES
+		#print $_->source_tag."\t";
+		}
+}
+
+
+########MUTATOR- THIS TAKES THE CODONS- THE CURRENT CODON AND RETURNS A MUTATION
+
+sub mutator {
+
+my ($codons,$codon,$pos,$rec_s,$rec_e,$pep_s,$pep_e)=@_;
+
+my @mutated=();
+
+#print "Considering gene range ".$$rec_s."\t".$$rec_e."\t peptide range ".$$pep_s."\t".$$pep_e."\n";
+
+foreach (@$codons){
+	if ($_ !~ m/$$codon/i){
+		#print $_." is not ".$$codon."\n";
+		my $cod=$_;
+		my @s1 = split //,$$codon;
+		my @s2 = split //,$_;
+
+		my $i = 0;
+			foreach  (@s1) {
+				my $mut=$s2[$i];
+    			if ($_ !~ m/$mut/i) {
+    				my $position=$i+$$pos+$$pep_s;
+       			 	#print "$_, $s2[$i] $i $$pos $$pep_s\t".$position."\n";
+       			 	
+       			 	if ($position >= $$rec_s && $position <= $$rec_e){
+       			 	#	print "GOOD SITE\n";
+       			 		my @tmp=();
+       			 		push (@tmp,$$codon);
+       			 		push (@tmp,$cod);
+       			 		push (@tmp,$s2[$i]);	
+       			 		push (@tmp,$position);
+       			 		push (@mutated,@tmp);
+       			 		}
+       			 		else{
+       			 	#		print "BAD SITE\n";
+       			 			}
+    				}
+   				 $i++;
+			}
+		}
+	}
+return @mutated;
+}
+
+
+
+
+
+
+
+
+
+
 ######RECOGNITION SITES#########
+# THIS SCRIPT FINDS THE LOCATION OF THE RECOGNITION SITE IN AN EXTENDED FRAGMENT OF DNA
+# NOTE- THIS TAKES AN ARBITRARY REGION SURROUNDING THE CUT SITE OF 12 BP EITHER SIDE
+
 sub recognition_sites{
 my ($seqobjects, $custom_collection)=@_;
 
 foreach (@$seqobjects){
-	#print $_->source_tag."\t";
+	#print "RECOGNITION SITES\n";
 	my $tag=$_->source_tag;
 	print $tag."\n";
 	#print $_->primary_tag."\t";
@@ -95,25 +261,38 @@ foreach (@$seqobjects){
 	my $subseq=$_->entire_seq()->subseq($f_left,$f_right);
 	print $subseq."\n";
 	print "Searching for ".$recog. " and ".$revcom_recog."\n";
+	my $strand=0;
 	
 	if ($subseq=~ /$recog/){
                 print "Forward found index location: $-[0]-$+[0]\n";
+                $strand =1;
+                $f_right=$f_left+$+[0]-1;
+                $f_left=$f_left+$-[0];
         }
          elsif ($subseq=~ /$revcom_recog/){
                 print "Revcom found index location: $-[0]-$+[0]\n";
+                $strand=-1;
+                $f_right=$f_left+$+[0]-1;
+                $f_left=$f_left+$-[0];
         }
          else {
                 print "came in else\n";
         }
 	
+	print $f_left."\t".$f_right."\n";
+	#my $sanitycheck=$_->entire_seq()->subseq($f_left,$f_right);
+	#print $sanitycheck."\n";
+	$_->add_tag_value('three',$f_left);
+	$_->add_tag_value('four',$f_right);
+	$_->strand($strand)
 	}
-
-
 }
 
 
 
 ####GENERATE SEQ OBJECTS######
+#THIS INSTANTIATES THE GENERIC SEQUENCE OBJECTS THAT ARE SUBSEQUENTLY USED TO STORE
+#EVERYTHING
 
 sub generate_objects{
 my ($enz,$ra,$loc)=@_;
@@ -132,7 +311,6 @@ my @seqobjects=();
 			#print "HERE ".$values[$i]."\n";
 			#print "HERE ".$values[$i+1]."\n";
 			
-			
 				my $feat = Bio::SeqFeature::Generic->new( 
  		 	           -primary      => 'restriction', # -primary_tag is a synonym
  		   	           -source_tag   => $enz,
@@ -141,7 +319,8 @@ my @seqobjects=();
         			   -tag          => { 'one' => $values[$i],
           			                      'two' => $values[$i+1] } );
           			$feat->attach_seq($$loc);                      
-          			#print $$loc->seq;
+          			 $feat->start($$loc->start);
+          			 $feat->end($$loc->end);
           			 
 				push (@seqobjects,$feat);
 		}
@@ -201,7 +380,7 @@ foreach (@$enzymes){
 foreach (keys %hash){
 	#print "STORED CUTS BY $_ ";
 	my @cuts=@{$hash{$_}};
-	#print join "\t", @cuts;
+	#print join "\t", @cuts;§
 	#print "\n";
 }
 return %hash
@@ -211,49 +390,81 @@ return %hash
 
 
 ######CONVERT COORDINATES DNA TO PROT##########
-#sub coord_convert{
-#my ($locatable_seq,$input_prot,$map_start,$map_end)=@_;
+sub coord_convert{
+my ($seqs)=@_;
 
 ####SEQUENCE COORDINATE CONVERSION FROM DNA TO PROTEIN
 
-#print "SEQUENCE COORDINATES \n\n";
-#print "GENE \t";
-#print $$locatable_seq->start()."\t";
-#print $$locatable_seq->end()."\n";
-#print "PROT \t";
-#print $$input_prot->start()."\t";
-#print $$input_prot->end()."\n";
-#print "GENEMAP\t";
-#print $$map_start."\t";
-#print $$map_end."\n";
+foreach (@$seqs){
+	my $input_prot=$_->entire_seq()->translate();
+	my @three= $_->get_tag_values('three');
+	my @four= $_->get_tag_values('four');
+	#print $input_prot->seq;
+	print "SEQUENCE COORDINATES \n\n";
+	print "GENE \t";
+	print $_->start."\t";
+	print $_->end."\n";
+	print "PROT \t";
+	print $input_prot->start()."\t";
+	print $input_prot->end()."\n";
+	print "GENEMAP\t";
+	print $three[0]."\t";
+	print $four[0]."\n";
 
-#COORDINATE MAPPING
+	#COORDINATE MAPPING
 
-#my $input_coordinates = Bio::Location::Simple->new  
-#  (-seq_id => 'cds', -start => $$locatable_seq->start(), -end => $$locatable_seq->end(), -strand=>1 );
-#my   $output_coordinates = Bio::Location::Simple->new  
-#	(-seq_id => 'pep', -start => $$input_prot->start(), -end => $$input_prot->end(), -strand=>1 );
-#my  $pair = Bio::Coordinate::Pair->new
-#  (-in => $input_coordinates ,  -out => $output_coordinates);
-#my  $pos = Bio::Location::Simple->new (-start => $$map_start, -end => $$map_end );
+	my $input_coordinates = Bio::Location::Simple->new  
+	  (-seq_id => 'cds', -start => $_->start, -end => $_->end(), -strand=>$_->strand() );
+	my   $output_coordinates = Bio::Location::Simple->new  
+		(-seq_id => 'pep', -start => $input_prot->start(), -end => $input_prot->end(), -strand=>$_->strand() );
+	my  $pair = Bio::Coordinate::Pair->new
+	  (-in => $input_coordinates ,  -out => $output_coordinates);
+	my  $pos = Bio::Location::Simple->new (-start =>  $three[0]-1, -end =>  $four[0]-1);
 
-# create a gene mapper and set it to map from chromosomal to cds coordinates
+	# create a gene mapper and set it to map from chromosomal to cds coordinates
 
-#my $gene = Bio::Coordinate::GeneMapper->new(-in   =>'cds',
-#                                      -out  =>'peptide',
-#                                      -cds  =>$$locatable_seq,
-#                                     );
+	my $gene = Bio::Coordinate::GeneMapper->new(-in   =>'cds',
+	                                      -out  =>'peptide',
+	                                      -cds  =>$_->entire_seq(),
+	                                     );
                                      
-#my $newloc = $gene->map($pos);
-#my $con_start = $newloc->start;
-#my $con_end = $newloc->end;
+	my $newloc = $gene->map($pos);
+	my $con_start = $newloc->start;
+	my $con_end = $newloc->end;
 
-#print "MAP\t";
-#print $con_start."\t";
-#print $con_end."\n";
-#return ($con_start,$con_end);
+	print "MAP\t";
+	print $con_start."\t";
+	print $con_end."\n";
+	$_->add_tag_value('five',$con_start);
+	$_->add_tag_value('six',$con_end);
+	
+	my $gene_back = Bio::Coordinate::GeneMapper->new(-in   =>'peptide',
+	                                      -out  =>'cds',
+	                                      -cds  =>$input_prot,
+	                                     );
+    my $pos_back = Bio::Location::Simple->new (-start =>  $con_start, -end =>  $con_end );                          
+	my $newloc_back = $gene_back->map($pos_back);
+	my $pep_start = $newloc_back->start;
+	my $pep_end = $newloc_back->end;
+	print "PEP\t";
+	print $pep_start."\t";
+	print $pep_end."\n";
+	$_->add_tag_value('seven',$pep_start);
+	$_->add_tag_value('eight',$pep_end);
+	my $frame_shift= $pep_start- $three[0];
+	print "FRAME \t". $frame_shift."\n";
+	$_->add_tag_value('nine',$frame_shift);
+	$_->add_tag_value('ten',$input_prot);
+	my $small_peptide=$input_prot->subseq($con_start,$con_end);
+	$_->add_tag_value('eleven',$small_peptide);
+	print "PEPTIDE \t". $small_peptide."\n";
+	my $small_seq=$_->entire_seq()->subseq($pep_start,$pep_end);
+	print "NUC SEQ \t". $small_seq."\n";
+	$_->add_tag_value('twelve',$small_seq);
+	
+	}
+}
 
-#}
 
 exit;
 
@@ -290,6 +501,36 @@ print "Multiple digest fragment lengths: ", join(' & ', map {length $_} @fragmen
 
   
 
+#primary      
+#restriction
+#source_tag   
+#enzyme name
+#strand
+#orientation of the recognition site	   	           		   	           
+#one 
+#start of the cut site
+#two
+#end of the cut site
+#three
+#start of the recognition site
+#four
+#end of the recognition site
+#five
+#protein start
+#six
+#protein end
+#seven
+#peptide start on DNA sequence
+#eight
+#peptide end on DNA sequence
+#nine
+#FRAME of DNA/PROTEIN (e.g. -2 is where the peptide starts 2bp before the recognition site
+##ten
+#PROTEIN SEQ
+#eleven
+#PEPTIDE
+#twelve
+#NUCLEOTIDE IN FRAME (within which is recognition site)
 
 
   
